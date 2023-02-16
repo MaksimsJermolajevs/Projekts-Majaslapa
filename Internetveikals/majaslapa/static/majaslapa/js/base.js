371dbca9eaa4f023aa1b2334008927027980f68e
EmilsBlinds <78017398+EmilsBlinds@users.noreply.github.com>
2023-02-17 01:57:01 +0200

    Update base.js

--- Internetveikals/majaslapa/static/majaslapa/js/base.js
+++ Internetveikals/majaslapa/static/majaslapa/js/base.js
@@ -1,1 +1,27 @@
-console.log('Hello')
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#checkbox');
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkMode', null);
+}
+ 
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('change', () => {
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  } else {  
+    disableDarkMode(); 
+  }
+});
